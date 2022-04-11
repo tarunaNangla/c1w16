7f1901c46d6037057de17a72ccb9427354a937b8
tarunaNangla <tarunanangla131@gmail.com>
2022-04-11 13:20:57 +0530

    cc
    cc

--- src/components/Grocerydetails.jsx
+++ src/components/Grocerydetails.jsx
@@ -10,6 +10,40 @@ console.log(data)
         <h1>Groceries</h1>
         <div className="container">
             {/* map through the data and display the cards */}
+            <ol>
+                <div className="leftside" >
+                   
+                    TOP DEALS
+                  
+                    </div>
+                {
+                    data.map( (ele)=>{
+                     return(
+                            <>
+                            <div className="productsdata">
+                            <div>
+
+<img src={ele.imgURL} alt="" />
+<h6>{ele.title}</h6>
+<div className="flexdata">
+<p>{ele.sellingPrice}</p>
+<p>M.R.P</p>
+<s>{ele.mrp}</s>
+</div>
+<div className="flexdata1" >
+    <p>Add to Cart</p>
+    <button>+</button>
+</div>
+</div>
+
+                            </div>
+                            </>
+                     )
+    
+                    })
+
+                }
+            </ol>
         </div>
         </>
     )
